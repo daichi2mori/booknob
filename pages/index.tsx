c97ecab8e3d56cec1779cfd8bada3683023cc8ed
daichi2mori <daichi2mori@gmail.com>
2022-11-22 14:49:29 +0900

    TailwindとMantine動作確認

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import { Button, Input } from '@mantine/core';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Book nob</h1>
+      <div className='flex gap-2'>
+        <Input placeholder='your name' />
+        <Button>Start</Button>
+      </div>
     </div>
   );
 };
